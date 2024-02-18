bdbe97d4ab6ec50f70be1c543d96b8d78861827c
10Hr <10hourmail@gmail.com>
2024-02-18 18:52:26 -0500

    added RewardValue

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,17 @@
+
+public class RewardValue {
+
+    private double cashValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public double getMilesValue() {
+        return cashValue * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+}
